24ba0f9ee5c4f270e301c169a430ae1ca3968684
yiguangchao <yiguangchao@feinno.com>
2020-11-26 19:51:53 +0800

    add leetcode1480.java

---
+++ src/Leetcode/leetcode1480.java
@@ -0,0 +1,13 @@
+package Leetcode;
+
+public class leetcode1480 {
+	
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i<nums.length; ++i){
+            nums[i] += nums[i-1];
+        }
+
+        return nums;
+    }
+	
+}
