a1bc9b33967752707e2dbdc53b588a3fb1c23115
yiqun <1241354012@qq.com>
2021-06-17 22:36:27 +0800

    add leetcode16

---
+++ src/Leetcode/leetcode16.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class leetcode16 {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestNum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1, r = nums.length - 1;
+            while (l < r){
+                int threeSum = nums[l] + nums[r] + nums[i];
+                if (Math.abs(threeSum - target) < Math.abs(closestNum - target)) {
+                    closestNum = threeSum;
+                }
+                if (threeSum > target) {
+                    r--;
+                } else if (threeSum < target) {
+                    l++;
+                } else {
+                    return target;
+                }
+
+            }
+
+        }
+
+        return closestNum;
+    }
+
+}
