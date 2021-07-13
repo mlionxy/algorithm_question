c7fe9655658f014f71686ce7f2473ddd0e5746cf
yiqun <1241354012@qq.com>
2021-07-13 18:22:08 +0800

    update 53

--- src/Leetcode/leetcode53.java
+++ src/Leetcode/leetcode53.java
@@ -6,11 +6,11 @@ class leetcode53 {
         int sum = 0;
         for(int num: nums) {
             if(sum > 0) {
-                sum += num;
+                sum += num;//计算累计值
             } else {
                 sum = num;
             }
-            ans = Math.max(ans, sum);
+            ans = Math.max(ans, sum);//比较最大值
         }
         return ans;
     }
