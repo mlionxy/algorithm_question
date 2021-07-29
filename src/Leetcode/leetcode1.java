f3b987b082b3822c32b7a74314682856cac4ed5f
yiqun <1241354012@qq.com>
2021-07-29 23:11:11 +0800

    update leetcode1

--- src/Leetcode/leetcode1.java
+++ src/Leetcode/leetcode1.java
@@ -5,10 +5,12 @@ public class leetcode1 {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
+			
             if(map.containsKey(nums[i])){
                 return new int[]{map.get(nums[i]), i};
             }
             map.put(target - nums[i], i);
+			
         }
         return null;
     }
