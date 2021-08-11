caba5dddf02db1ebbf613ad6e46fad6ee5d715c1
yiqun <1241354012@qq.com>
2021-08-11 23:42:13 +0800

    add leetcode

--- src/Leetcode/leetcode1.java
+++ src/Leetcode/leetcode1.java
@@ -1,16 +1,17 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class leetcode1 {
 
+    //哈希解法，从哈希表中查找是否有对应的另一个值
+    //时间复杂度O(N) 空间复杂度O(N)
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-			
-            if(map.containsKey(nums[i])){
-                return new int[]{map.get(nums[i]), i};
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
             }
-            map.put(target - nums[i], i);
-			
+            hashtable.put(nums[i], i);
         }
         return null;
     }
